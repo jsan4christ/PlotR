6264eb4b1aef91fe85027d508e217b7fca0a52d1
mr664 <m.ravenhall@live.co.uk>
2015-12-01 12:06:16 +0000

    Added file specification for plotSNPIntensity.r

--- plotSNPIntensity.r
+++ plotSNPIntensity.r
@@ -1,36 +1,50 @@
-##########################################################################################################################
-# plotSNPIntensity.r - Matt Ravenhall											 #
-# Provided an array of SNPs has been provided at the command line as shown below, produces SNP intensity plots for each. #
-# e.g. 'plotSNPIntensity.r CHR:BP' will produce Case, Control & Combined SNP intensity plots.				 #
-# Note that this script assumed out.XY.format exists within the working directory and contains XY data.			 #
-##########################################################################################################################
+#################################################################################
+# plotSNPIntensity.r - Matt Ravenhall						#
+# Given an array of SNPs as shown below, produce SNP intensity plots for each. 	#
+# e.g. 'Rscript plotSNPIntensity.r CHR1:BP1 CHR2:BP2 CHR3:BP3'	 		#
+#################################################################################
 
-# Warning: This script should now take CHR:BP instead of BP input. Testing now to see if that works.
+# Note: Currently XYfile is subsetted for each SNP, this could be improved with pre-loop subsetting by chromosome
 
 # Optional
-xlim.fixed=2
-ylim.fixed=2
+xlim.fixed <- 2
+ylim.fixed <- 2
+sepChar <- ':'
+XYfile <- 'out.XY.FORMAT' # Requires POS (position), CHR (chromosome) and XY (intensity as X,Y) columns
+GTfile <- 'out.GT.FORMAT' # Requires POS (position), CHR (chromosome) and colID columns
+CCfile <- '../RawData/Tanzania_GWAS-2.5M_b37_915.sample' # Requires 'scanID' (IDs) and 'caseorcontrol' ('CASE'/'CONTROL') columns
 
 # Grab Position (in BP) of SNP
 args <- commandArgs(trailingOnly=TRUE)
 
 # Bring in all SNPs
 print('Parsing XY Data')
-dat <- read.table('out.XY.FORMAT',header=T)
+dat <- read.table(XYfile,header=T)
 
 # Loop SNPs, currently probably not the most efficient approach but it'll work
 for (eachsnp in args) {
+
+	# Skip if separation character is not in a given argument
+	if (!grepl(sepChar, eachsnp)) {
+		warning(paste('Skipping ',eachsnp,' due to missing separation character (',sepChar,').',sep=''))
+		break 
+	}
+
 	# Use i for BP, j for CHR
 	i <- strsplit(eachsnp, ':')[[1]][2]
 	j <- strsplit(eachsnp, ':')[[1]][1]
 
 	# Isolate SNP of interest
 	print(paste('Isolating XY data for SNP on CHR ',j,' at BP ',i,sep=''))
-#	snp <- data.frame(dat[dat$CHR == j,]))
-#	snp <- data.frame(t(dat[dat$POS == i,]))
 	snp <- data.frame(t(dat[dat$POS == i & dat$CHR == j,]))
 	print('Removing CHROM and POS data from isolated subset')
 	snp <- subset(snp, row.names(snp) != 'CHROM' & row.names(snp) != 'POS')
+
+	# Check the SNP, and only one version of that SNP, exists in dataset
+	if (dim(snp)[2] != 1) {
+		warning(paste('Skipping ',eachsnp,' due to missing or duplicate value in XY dataset.',sep=''))
+		break
+	}
 
 	# Split XY columns
 	names(snp) <- c('XY')
@@ -40,9 +54,9 @@ for (eachsnp in args) {
 
 	# Add colours
 	print('Parsing GT data (for point colours)')
-	cols <- read.table('out.GT.FORMAT',header=T)
+	cols <- read.table(GTfile,header=T)
 	print(paste('Isolating GT data for SNP on CHR ',j' at BP ',i,sep=''))
-	snpcol <- data.frame(t(cols[cols$POS == i,]))
+	snpcol <- data.frame(t(cols[cols$POS == i & cols$CHR == j,]))
 	print('Removing CHROM and POS data from isolated subset')
 	snpcol <- subset(snpcol, row.names(snpcol) != 'CHROM' & row.names(snpcol) != 'POS')
 
@@ -57,23 +71,23 @@ for (eachsnp in args) {
 
 	# Identify IDs as Case/Control
 	print('Parsing phenotypic data')
-	pheno <- read.table('../RawData/Tanzania_GWAS-2.5M_b37_915.sample',header=T)
+	pheno <- read.table(CCfile,header=T)
 	print('Identifying Case & Control individuals')
-	CaseIDs <- pheno$scanID[pheno$caseorcontrol=='CASE']
-	ControlIDs <- pheno$scanID[pheno$caseorcontrol=='CONTROL']
+	CaseIDs <- pheno$scanID[toupper(pheno$caseorcontrol)=='CASE']
+	ControlIDs <- pheno$scanID[toupper(pheno$caseorcontrol)=='CONTROL']
 
 	# Plot to file
-	print(paste('Plotting figure as SNPIntensity_BP',i,'.png',sep=''))
+	print(paste('Plotting figure as SNPIntensity_',j,':',i,'_Combined.png',sep=''))
 	png(paste('SNPIntensity_',j,':',i,'_Combined.png',sep=''))
 	plot(snp$X, snp$Y, xlim=c(0,xlim.fixed), ylim=c(0,ylim.fixed), col=rainbow(4)[1 + as.numeric(snpcol$colID)], pch=16, main=paste(j,':',i,' Intensity (Case+Control)',sep=''), xlab='X', ylab='Y')
 	dev.off()
 
-	print(paste('Plotting figure as SNPIntensity_BP',i,'_Case.png',sep=''))
+	print(paste('Plotting figure as SNPIntensity_',j,':',i,'_Case.png',sep=''))
 	png(paste('SNPIntensity_',j,':',i,'_Case.png',sep=''))
 	plot(snp$X[CaseIDs], snp$Y[CaseIDs], xlim=c(0,xlim.fixed), ylim=c(0,ylim.fixed), col=rainbow(4)[1 + as.numeric(snpcol$colID[CaseIDs])], pch=16, main=paste(j,':',i,' Intensity (Case Only)',sep=''), xlab='X', ylab='Y')
 	dev.off()
 
-	print(paste('Plotting figure as SNPIntensity_BP',i,'_Control.png',sep=''))
+	print(paste('Plotting figure as SNPIntensity_',j,':',i,'_Control.png',sep=''))
 	png(paste('SNPIntensity_BP',j,':',i,'_Control.png',sep=''))
 	plot(snp$X[ControlIDs], snp$Y[ControlIDs], xlim=c(0,xlim.fixed), ylim=c(0,ylim.fixed), col=rainbow(4)[1 + as.numeric(snpcol$colID[ControlIDs])], pch=16, main=paste(j,':',i,' Intensity (Control Only)',sep=''), xlab='X', ylab='Y')
 	dev.off()
